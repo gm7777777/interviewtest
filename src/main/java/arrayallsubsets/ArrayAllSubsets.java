04fd7470df7d60b50a52b1708c37cd34e729f336
gm7777777 <119618949@qq.com>
2021-08-07 12:54:20 +0800

    数组子查询

--- src/main/java/arrayallsubsets/ArrayAllSubsets.java
+++ src/main/java/arrayallsubsets/ArrayAllSubsets.java
@@ -9,9 +9,9 @@ import java.util.Set;
 public class ArrayAllSubsets {
 
     public static void main(String[] args) {
-//        int[] nums = {1,3,4,5,7,8,9,6,3};
+        int[] nums = {1,3,4,5,7,8,9,6,3};
 //        int[] nums = {1,3,4};
-        int[] nums = {1,3,4};
+//        int[] nums = {1,3,4};
         Set<Set<Integer>> res = new HashSet<>();
         getRestGroup(res, new HashSet(),0,nums);
 
@@ -19,31 +19,17 @@ public class ArrayAllSubsets {
     }
 
     public static void getRestGroup(Set<Set<Integer>> alllist , Set grouplist, int index, int[] sums){
-//        if(index<=sums.length){
+
             alllist.add(new HashSet<>(grouplist));
-//        }
-//        alllist.add(new ArrayList<>(grouplist));
+
+
         for(int i = index;i<sums.length;i++){
             int t = sums[i];
             Set tgrouplist = (Set) ((HashSet)grouplist).clone();
             tgrouplist.add(t);
 
-//            if(!alllist.isEmpty()){
-//                ArrayList<ArrayList> tempAll = (ArrayList) alllist.clone();
-//                for(ArrayList tempList :tempAll){
-////                    if((tempList.size())==index&&(tempList.size()>0&&(int)tempList.get(tempList.size()-1)!=t)){
-//                    if((tempList.size())==index){
-//                        tempList = new ArrayList<>(tempList);
-//                        tempList.add(t);
-//                        getRestGroup(alllist,tempList,index+1,sums);
-//                    }
-//                }
-//            }
-//            if (grouplist.size()>1&&t==(int)grouplist.get(grouplist.size()-2)) {
-//                grouplist.remove(grouplist.size()-1);
-//            }
             getRestGroup(alllist,tgrouplist,index+1,sums);
-//            grouplist.remove(grouplist.size()-1);
+
         }
 
     }
